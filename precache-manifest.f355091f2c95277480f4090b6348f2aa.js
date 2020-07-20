cfc3f9bcbf01ec21b6d035e9cd2101b02cc357cb
Giuli <giulicecchetto@gmail.com>
2020-07-20 20:34:42 -0300

    Updates

---
+++ precache-manifest.f355091f2c95277480f4090b6348f2aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b93479ba855ac102816b5fefd3f6304",
+    "url": "/workout-planner/index.html"
+  },
+  {
+    "revision": "b23936a2c836601f7e47",
+    "url": "/workout-planner/static/css/main.9abf05af.chunk.css"
+  },
+  {
+    "revision": "02a2f65258c4c8d7b723",
+    "url": "/workout-planner/static/js/2.a727281b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/workout-planner/static/js/2.a727281b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23936a2c836601f7e47",
+    "url": "/workout-planner/static/js/main.b5de8c2d.chunk.js"
+  },
+  {
+    "revision": "35abf5e4ff8f20a0da7f",
+    "url": "/workout-planner/static/js/runtime-main.06edf704.js"
+  },
+  {
+    "revision": "f4685b52e7e9d2aa48772b9d512420d0",
+    "url": "/workout-planner/static/media/background.f4685b52.png"
+  },
+  {
+    "revision": "d1442d476df03f46b75df668a4d99758",
+    "url": "/workout-planner/static/media/card-detail1.d1442d47.png"
+  }
+]);
